da29f797886a6afa36bb5383ab0cb5875d25325b
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-14 20:03:16 -0300

    Primeira diretiva: *ngFor="let photo of photos"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Rick and Morty';
   
+  photos = [
+    {
+      url:'https://rollingstone.uol.com.br/media/_versions/rick-morty-temporada-4_widelg.jpg',
+      description:'rickAndMorty'
+    },
+    {
+      url:'https://uploads.metropoles.com/wp-content/uploads/2019/10/07112336/rick-and-morty-adult-swim-450x300.jpg',
+      description:'rickAndMorty2'
+    }
+  ];
+  
+
 }
--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PhotoComponent {
 
-  @Input() description = '';
   @Input() url = '';
+  @Input() description = '';
 
 }
