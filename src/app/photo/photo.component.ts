6df4127fa8e1fafcf35cd1cc7f36645d45df430e
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-14 19:22:08 -0300

    Criando componente PHOTO e utilizando o conceito de inbound property

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent {
+
+  @Input() description = '';
+  @Input() url = '';
+
+}
