84f7ea6e991099a51301d36abfae969179156406
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-15 10:39:18 -0300

    Ajuste AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,18 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Rick and Morty';
-  
-  photos = [
-    {
-      url:'https://rollingstone.uol.com.br/media/_versions/rick-morty-temporada-4_widelg.jpg',
-      description:'rickAndMorty'
-    },
-    {
-      url:'https://uploads.metropoles.com/wp-content/uploads/2019/10/07112336/rick-and-morty-adult-swim-450x300.jpg',
-      description:'rickAndMorty2'
-    }
-  ];
-  
 
 }
