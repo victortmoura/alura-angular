91046b0e5b583f2cbd51b1ab3569cb8051e8b8ca
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-15 10:39:39 -0300

    Criação PhotoListComponent

---
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+  photos: Photo[] = [];
+  
+  constructor(private photoService: PhotoService) {}
+  
+  ngOnInit(): void {
+    this.photoService.listFromUser('flavio')
+      .subscribe(photos => {
+        console.log(photos[0].description); 
+        this.photos = photos;
+      });
+  }
+
+}
