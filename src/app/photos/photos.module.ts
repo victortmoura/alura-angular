f5454f6e286043d59eeae2e6e02e331a2b16cf4f
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-15 11:02:03 -0300

    Adicionando CommomModule em PhotosModule para ter acesso as diretivas

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 
 import { PhotoComponent } from './photo/photo.component';
@@ -11,7 +12,8 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
         PhotoListComponent
       ],
       imports: [
-        HttpClientModule
+        HttpClientModule,
+        CommonModule
       ]
 })
 export class PhotosModule {}
