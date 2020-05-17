71bb7c96d9b27bf5ad954558116e3527967f4548
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-17 16:42:01 -0300

    Ajuste no PhotoListComponente TS e HTML pra filtrar fotos usando PIPE

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -11,6 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
+  filter: string = '';
   
   constructor(
     private photoService: PhotoService,
