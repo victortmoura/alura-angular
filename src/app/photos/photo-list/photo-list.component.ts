8b30192e1d14ef15c489db887b03633336b86b5e
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-18 17:13:18 -0300

    RxJS e seu Subject: implementando o conceito e objeto

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
   
   constructor(private activatedRoute: ActivatedRoute) {}
   
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
+
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
+  
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
 }
