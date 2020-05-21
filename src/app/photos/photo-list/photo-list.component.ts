5d93d01729c554a5c39462d2d7acbb76b3107aab
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-21 19:05:42 -0300

    Remoção do debounce do PhotoList por ter sido componentizado em Search

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import { PhotoService } from '../photo/photo.service';
 
 
 @Component({
@@ -11,24 +10,31 @@ import { debounceTime } from 'rxjs/operators';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
   
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+    ) {}
   
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
-
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter = filter);
+    this.userName = this.activatedRoute.snapshot.params.userName;
   }
-  
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
+
+  load(){
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+        .subscribe(photos => {
+          this.photos = this.photos.concat(photos);
+          if(!photos.length) this.hasMore = false;
+        });
   }
 
 }
