d2c91a0a0733c900fb381ca0cfa415eeb73c8ee1
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-21 19:04:17 -0300

    Organizando componentes nos seus respectivos módulos

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotosComponent } from './photos/photos.component';
+import { FilterByDescription } from './filter-by-description.pipe';
+import { PhotoModule } from '../photo/photo.module';
+import { CardModule } from 'src/app/shared/components/card/card.module';
+import { SearchComponent } from './search/search.component';
+
+@NgModule({
+    declarations: [
+        PhotoListComponent,
+        PhotosComponent,
+        LoadButtonComponent,
+        FilterByDescription,
+        SearchComponent        
+    ],
+    imports: [
+        CommonModule,
+        PhotoModule,
+        CardModule
+    ]
+})
+export class PhotoListModule {
+
+}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,23 +2,18 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
-
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoModule } from './photo/photo.module';
 
 @NgModule({
     declarations: [
-        PhotoComponent,
-        PhotoListComponent,
-        PhotoFormComponent,
-        PhotosComponent,
-        FilterByDescription
+
       ],
       imports: [
-        HttpClientModule,
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
         CommonModule
       ]
 })
