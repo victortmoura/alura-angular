08119c3a0a84ae6439943cdb20679c38d475137e
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-22 16:42:09 -0300

    Aplicando diretiva no módulo requisitado e no html

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -7,7 +7,8 @@ import { PhotosComponent } from './photos/photos.component';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from 'src/app/shared/components/card/card.module';
-import { SearchComponent } from './search/search.component';
+import { SearchComponent } from './search/components/search.component';
+import { DarkenOnHoverModule } from './search/directives/darken-on-hover/darken-on-hover.module';
 
 @NgModule({
     declarations: [
@@ -20,7 +21,8 @@ import { SearchComponent } from './search/search.component';
     imports: [
         CommonModule,
         PhotoModule,
-        CardModule
+        CardModule,
+        DarkenOnHoverModule
     ]
 })
 export class PhotoListModule {
