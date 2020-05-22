e5d65467f95ced7cc5e2f3463eae54773c580ad2
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-22 19:42:44 -0300

    Atualizando módulos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [
         SigninComponent
+    ], 
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule
     ]
 })
 export class HomeModule { }
--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -8,7 +8,7 @@ import { FilterByDescription } from './filter-by-description.pipe';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from 'src/app/shared/components/card/card.module';
 import { SearchComponent } from './search/components/search.component';
-import { DarkenOnHoverModule } from './search/directives/darken-on-hover/darken-on-hover.module';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
 
 @NgModule({
     declarations: [
