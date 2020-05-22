24e29d9ef39d6fbea8e868860cf041aded64e322
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-22 19:42:31 -0300

    Alterando caminho da DIRETIVA DarkenOnHover para a pasta SHARED

--- src/app/photos/photo-list/search/directives/darken-on-hover/darken-on-hover.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, ElementRef } from '@angular/core';
-
-@Directive({
-    selector: '[apDarkenOnHover]'
-})
-export class DarkenOnHoverDirective {
-
-    constructor(private el: ElementRef) {}
-}
