1a37c0e4075efeb15d2a2c4e5bf97f5f1059543d
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-24 16:09:26 -0300

    Ajustes SigninComponent autenticando/redirecionando o usuário para home.

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platfomr-detector.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -7,8 +11,13 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SigninComponent implements OnInit {
 
     loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
-    constructor(private formBuilder: FormBuilder){}
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService){}
     
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
@@ -17,5 +26,21 @@ export class SigninComponent implements OnInit {
         })
     }
     
+    login() {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService.authenticate(userName, password)
+            .subscribe(
+                () => this.router.navigate(['user', userName]),
+                err => {
+                    console.log(err);
+                    this.platformDetectorService.isPlatformBrowser() && 
+                        this.userNameInput.nativeElement.focus();
+                    alert('user name is incorrect !');
+                    this.loginForm.reset();
+                } 
+            );
+    }
     
 }
