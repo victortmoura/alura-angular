cbe4d917234e363a5d198f182134d9ca3d9a3b43
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-27 19:21:28 -0300

    Guarda de rotas

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -11,6 +11,7 @@ import { User } from './user';
 export class UserService {
 
   private userSubject = new BehaviorSubject<User>(null);
+  private userName: string;
 
   constructor(private tokenService: TokenService) { 
     this.tokenService.hasToken() && 
@@ -29,11 +30,20 @@ export class UserService {
   private decodeAndNotify() {
     const token = this.tokenService.getToken();
     const user = jwt_decode(token) as User;
+    this.userName = user.name;
     this.userSubject.next(user);
   }
 
   logout() {
     this.tokenService.removeToken();
     this.userSubject.next(null);
+  }
+
+  isLogged() {
+    return this.tokenService.hasToken();
+  }
+
+  getUserName(){
+    return this.userName;
   }
 }
