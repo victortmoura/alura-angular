ceb8bd5c69123ef7b935b4def39e491bc0820879
Victor Moura <jose.moura@hivecloud.com.br>
2020-05-27 20:01:58 -0300

    A diretiva routerLink

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
@@ -10,7 +11,8 @@ import { CommonModule } from '@angular/common';
         HeaderComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        RouterModule
     ]
 })
 export class CoreModule {}
